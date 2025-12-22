b246979071ad4263b7ed8527910e24fdaec2fc92
Andhi <andhi@gmail.com>
2025-12-22 14:54:45 +0700

    a

--- src/app/(Home)/layout.tsx
+++ src/app/(Home)/layout.tsx
@@ -1,13 +1,13 @@
-import Footer from "./components/footer";
-import Navbar from "./components/navbar";
+//import Footer from "./components/footer";
+//import Navbar from "./components/navbar";
 import { ReactNode } from "react";
 export default function LayoutHome({children}:{children:ReactNode})
 {
   return (
     <main className="w-screen h-screen">
-      <Navbar></Navbar>
+      
       {children}
-      <Footer></Footer>
+      
     </main>
   );
 }
