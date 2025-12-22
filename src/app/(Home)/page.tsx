d667a26c9e083b315b0786f7fe6e4de6cbe0efb7
Andhi <andhi@gmail.com>
2025-12-22 17:32:55 +0700

    a

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,11 +1,12 @@
 export default function Home(){
     return(
         <div>
-                <div className="min-h-screen w-full bg-green-600 flex items-center justify-center">
-      <h1 className="text-white text-4xl font-bold">
+        <div className="min-h-screen w-full bg-green-600 flex items-center justify-center">
+        <h1 className="text-white text-4xl font-bold">
         Hello World
-      </h1>
-    </div>
+        <div className="w-[100px] h-[100px] bg-pink-200 flex items-center justify-center"><div>a</div></div>
+        </h1>
+        </div>
         </div>
     )
 }
