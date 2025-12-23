ca1614f6013ed0308c1e7a6f3f87eaf9d23853d2
Andhi <andhi@gmail.com>
2025-12-23 18:10:51 +0700

    f

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,7 +1,23 @@
-export default function Home(){
-    return(
-        <div>
- <div id="hero"></div>
-        </div>
-    )
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Loading() {
+  const dots = ["Loading.", "Loading..", "Loading..."];
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % dots.length);
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="bg-green-400 h-screen flex justify-center items-center text-center">
+      <div className="bg-pink-100 h-[100px] w-[100px] flex items-center justify-center">
+        <div>{dots[index]}</div>
+      </div>
+    </div>
+  );
 }
