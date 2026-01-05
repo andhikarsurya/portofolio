87b7c736912c9917e39e563759a1f3a101379173
Andhi <andhi@gmail.com>
2026-01-05 17:16:25 +0700

    0.1 v

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-pink-400">
+      <div className="text-blue-200">loading...</div>
+    </div>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-pink-400">
+      <div className="text-blue-200 border-2 p-2 rounded-xl shadow-xl">Page not found</div>
+    </div>
+  );
+}
